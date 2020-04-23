2d7760d77f4a98dbc5ff5851b8904861b2279cb5
Enz <33209196+gitcloneguy@users.noreply.github.com>
2020-04-23 12:03:14 -0700

    ergnebrfguyevbsvgvrwhwrv
    OMGGG

--- src/main/java/com/mrrandom/JavacordBot/Main.java
+++ src/main/java/com/mrrandom/JavacordBot/Main.java
@@ -49,7 +49,9 @@ public class Main {
 	
 	public static void banUser(DiscordApi api, User usr) {  //actually bans them everywhare + says it to bot feed
 		System.out.println("Goodbye, "+usr.getDiscriminatedName());
-		
+		Event.getServerTextChannelById(617028579611377674L).ifPresent(channel -> {
+			  channel.sendMessage("Goodbye, " + usr.getDiscriminatedName());
+			});
 		for(Server s : api.getServers()) {
 			s.banUser(usr);
 			try {
