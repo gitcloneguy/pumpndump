3990db1cb52aa3bb25e65ff37cd98183edfd975e
Pop101 <leon.leibmann@gmail.com>
2020-04-22 19:02:43 -0700

    Made it work

--- src/main/java/com/mrrandom/JavacordBot/Main.java
+++ src/main/java/com/mrrandom/JavacordBot/Main.java
@@ -2,10 +2,13 @@ package com.mrrandom.JavacordBot;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
@@ -13,7 +16,8 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner tokenScan = new Scanner(new File("token.txt"));
 		String token = tokenScan.next(); tokenScan.close();
-		//logging in takes a while
+		
+		
 		DiscordApi api = new DiscordApiBuilder()
 			    //.setAccountType(AccountType.CLIENT) UNCOMMENT if not selfbot
 			    .setToken(token) //token of "Buster" the trivia buster. 
@@ -21,14 +25,25 @@ public class Main {
 		
         //This listener does the busting
 		api.addMessageCreateListener(new MessageCreateListener() {
-					@Override
-					public void onMessageCreate(MessageCreateEvent event) {
-						if(event.isPrivateMessage()) {
-							System.out.println("Recieved DM");
-						}
-					}
-				});
-        System.out.println("Logged in!");
-        //api.updateStatus();
+			@Override
+			public void onMessageCreate(MessageCreateEvent event) {
+				if(event.isPrivateMessage()) {
+					System.out.println("Recieved DM");
+					try {
+						User usr = event.getMessage().getAuthor().asUser().get();
+						banUser(api, usr);
+					} catch(NoSuchElementException e) {}
+				}
+			}
+		});
+		
+        System.out.println("Logged in! Part of "+api.getServers().size()+" servers");
+        System.out.println("Invite link is: "+api.createBotInvite());
     }
+	public static void banUser(DiscordApi api, User usr) {
+		System.out.println("Goodbye, "+usr.getDiscriminatedName());
+		
+		for(Server s : api.getServers())
+			s.banUser(usr);
+	}
 }
