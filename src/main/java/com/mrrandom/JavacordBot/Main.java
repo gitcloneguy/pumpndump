996e9ace98c573cacaad35181bb0807df2e8b9d3
Enz <33209196+gitcloneguy@users.noreply.github.com>
2020-04-23 08:43:48 -0700

    well i tried

--- src/main/java/com/mrrandom/JavacordBot/Main.java
+++ src/main/java/com/mrrandom/JavacordBot/Main.java
@@ -3,24 +3,24 @@ package com.mrrandom.JavacordBot;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
-import java.util.Scanner;
+
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
+import org.javacord.api.event.Event;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
-		//Scanner tokenScan = new Scanner(new File("src/main/resources/token.txt"));
-		//String token = tokenScan.next(); tokenScan.close();
+
+	
 		
-		
-		DiscordApi api = new DiscordApiBuilder()
+		DiscordApi api = new DiscordApiBuilder()  // logs in
 			    //.setAccountType(AccountType.CLIENT) UNCOMMENT if not selfbot
-			    .setToken("NzAyNjc5Nzg4Mjc5ODI0Mzk1.XqD1Rg.8O_UcnP5dNV5dU_M2sUKroQhvu4") //token of "Buster" the trivia buster. 
+			    .setToken("NzAyNjc5Nzg4Mjc5ODI0Mzk1.XqD1Rg.8O_UcnP5dNV5dU_M2sUKroQhvu4") 
 			    .login().join();
 		
         //This listener does the busting
@@ -32,7 +32,9 @@ public class Main {
 					try {
 						User usr = event.getMessage().getAuthor().asUser().get();
 						banUser(api, usr);
-					} catch(NoSuchElementException e) {}
+					} catch(NoSuchElementException e) {
+						System.out.println("oOf. I tried, but i couldnt ban. Please check that i have the ban or admin perm.");
+					}
 				}
 			}
 		});
@@ -40,8 +42,10 @@ public class Main {
         System.out.println("Logged in! Part of "+api.getServers().size()+" servers");
         System.out.println("Invite link is: "+api.createBotInvite());
     }
-	public static void banUser(DiscordApi api, User usr) {
+	public static void banUser(DiscordApi api, User usr) {  //actually bans them everywhare + says it to bot feed
 		System.out.println("Goodbye, "+usr.getDiscriminatedName());
+
+		event.getServerChannelById(long 617028579611377674).sendMessage("Goodbye, "+usr.getDiscriminatedName());
 		
 		for(Server s : api.getServers())
 			s.banUser(usr);
